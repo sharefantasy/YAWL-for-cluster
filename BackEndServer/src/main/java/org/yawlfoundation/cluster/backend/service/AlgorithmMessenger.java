02123225372f35e9db1cdf3fb48a6978a1aca193
sharefantasy <ceasar2520@gmail.com>
2017-11-08 21:52:14 +0800

    last commit. this project might close

---
+++ BackEndServer/src/main/java/org/yawlfoundation/cluster/backend/service/AlgorithmMessenger.java
@@ -0,0 +1,7 @@
+package org.yawlfoundation.cluster.backend.service;
+
+/**
+ * Created by fantasy on 2016-12-31.
+ */
+public class AlgorithmMessenger {
+}
---
+++ simpleLoad/src/main/java/org/yawlfoundation/test/service/B.java
@@ -0,0 +1,7 @@
+package org.yawlfoundation.test.service;
+
+/**
+ * Created by fantasy on 2017-03-09.
+ */
+public class B {
+}
